ab431ef0e43cb0ee540d5a4dee7a34c49c5262ea
unknown <daniel.oteroavalle@gmail.com>
2020-12-29 16:12:19 +0000

    Created branch oteroPOC to host prove of concept 1
    Created walls prefab. Static body to constrain player movement.
    Refactored player controller to use rigidbody instead of position
    transforms to account for collisions with static bodies.
    Created Point A and Point B objects for further POC development.

--- GOD/Assets/Scripts/PlayerController.cs
+++ GOD/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,27 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 10f;
 
-    private Vector3 movementInput = new Vector3( 0, 0, 0 );
+    private Vector2 movementInput;
+    private Rigidbody2D myRigidbody;
    
+    private void Start() {
+        movementInput = new Vector3( 0, 0, 0 );
+        myRigidbody = GetComponent< Rigidbody2D >();
+    }
+
     private void ReadInput() {
         // a w s d keys to move the player
         // alternatively arrow keys
-        movementInput = new Vector3( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ), 0 );
+        movementInput = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ) );
     }
 
     private void Move() {
         // move based on input
-        Vector3 direction = movementInput.normalized;
-        Vector3 velocity = direction * speed;
-        transform.position += Time.fixedDeltaTime * velocity;;
+        Vector2 direction = movementInput.normalized;
+        Vector2 velocity = direction * speed;
+
+        // need to move using rigidbody to account for collisions with static objects
+        myRigidbody.position += Time.fixedDeltaTime * velocity;;
     }
 
     private void Update() {
@@ -27,6 +35,20 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate() {
         Move(); 
+    }
+
+    private void OnTriggerEnter2D( Collider2D other ) {
+        switch ( other.name ) {
+            case "Point A":
+                Debug.Log( "You're back on point A, go to point B");
+                break;
+            case "Point B":
+                Debug.Log(  "Oh no, you're on point B. Hurry back to point A!" );
+                break;
+            default:
+                Debug.Log( "Ignore " + other.name );
+                break;
+        }
     }
 
 }
